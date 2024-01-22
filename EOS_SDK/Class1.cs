7b5617069b6eec4f51226ccc510c9be9cf7e2f35
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2024-01-22 18:38:24 +0100

    basic files

---
+++ EOS_SDK/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace EOS_SDK
+{
+    public class Class1
+    {
+        public static object Locker = new object();
+        [UnmanagedCallersOnly(EntryPoint = "_", CallConvs = new[] { typeof(CallConvCdecl) })]
+        public static int _()
+        {
+            lock (Locker)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EOS_DLL
+My attempt to make an EOS Server or smth.
